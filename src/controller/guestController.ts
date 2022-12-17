b513f9885c6da65b9e1f49f378ffbea3eeec581f
오 승하 <noshel8197@gmail.com>
2022-12-17 07:06:44 +0000

    Update src/controller/guestController.ts

--- src/controller/guestController.ts
+++ src/controller/guestController.ts
@@ -1,11 +1,11 @@
 import { userService } from '../services';
+
 import { AsyncRequestHandler } from '../types';
 import { user } from '../services/interfaces/interface';
 
 interface IGuestController {
   register: AsyncRequestHandler;
 }
-
 export class GuestController implements IGuestController {
   register: AsyncRequestHandler = async (req, res) => {
     const newUser: user = {
