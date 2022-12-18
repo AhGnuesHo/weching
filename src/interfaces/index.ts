3c809f4a7606346d2434755cc8b64a2cea5d7bf4
seungha <frazzle124@gmail.com>
2022-12-18 12:12:54 +0900

    궁금한거

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -52,6 +52,12 @@ enum point {
   POST = -3,
   REVIEW = 5,
 }
+
+// 질문 ) 인터페이스가 값과 행위가 분리되어 사용하고 있음
+// 위의 인터페이스들은 값만 정의되어있음 거의 타입처럼 사용하는 중이고
+// 아래의  IModel 인터페이스들은 행위 (주로 디비에서 데이터를 가져오는 행위)들로 정의 되어있음
+// 이렇게 분리가 되어도 되는게 맞는지... ?
+
 interface IReviewModel {
   todoReview(userId: number): Promise<newPost[]>;
   writeReview(review: review): Promise<Boolean>;
