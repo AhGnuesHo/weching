610f6bada779e60ac77ac69ad3df20ed485e7b07
seungha <frazzle124@gmail.com>
2022-12-20 12:01:29 +0900

    fix: 닉네임 중복 검사

--- src/middlewares/userHandler.ts
+++ src/middlewares/userHandler.ts
@@ -7,25 +7,44 @@ export async function userHandler(
   next: NextFunction
 ) {
   const email = req.body.email;
-  const nickName = req.body.nickName;
+  const nameState = req.body.nameState;
   try {
     const isUser = await userModel.isUser(email);
     if (isUser) {
       log.warn(`email ${email} already exists`);
-      throw new Error(`email ${email} already exists`);
-    }
-    const isNickName = await userModel.isNickName(nickName);
-    if (isNickName) {
-      log.warn(`nickname ${nickName} already exists`);
-      throw new Error(`nickname ${nickName} already exists`);
+      res.status(400).send({ message: `email ${email} already exists` });
     }
 
-    if (nickName.length > 12) {
-      log.warn('nickName is too long');
-      throw new Error('nickName is too long');
+    if (!nameState) {
+      log.warn(`중복 검사가 안된 닉네임`);
+      res.status(400).send({ message: '중복검사해주세요' });
     }
     next();
   } catch (err) {
     next(err);
   }
 }
+export const checkName = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const nickName = req.body.nickName;
+
+    const isNickName = await userModel.isNickName(nickName);
+    if (isNickName) {
+      log.warn(`nickname ${nickName} already exists`);
+      res.status(400).send({ message: ` ${nickName} is already exists` });
+    }
+
+    if (nickName.length > 12) {
+      log.warn('nickName is too long');
+      res.status(400).send({ message: ` nickName is too long` });
+    }
+
+    res.status(200).send({ message: ` ${nickName} is available` });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { asyncHandler } from '../utils';
 import { userController } from '../controller/userController';
+import { userHandler, checkName } from '../middlewares';
 
 export const userRouter = Router();
 
 userRouter.get('/', asyncHandler(userController.findUser));
 userRouter.delete('/', asyncHandler(userController.deleteUser));
+userRouter.post('/checkName', checkName);
