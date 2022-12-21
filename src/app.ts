d74feee3a778401bf1344a8280c3f37420608ca9
seungha <frazzle124@gmail.com>
2022-12-21 13:39:22 +0900

    cors

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,7 @@ pg.connect()
   .then(() => log.info(`database Connect`))
   .catch((err) => log.err('connection error', err.stack));
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
