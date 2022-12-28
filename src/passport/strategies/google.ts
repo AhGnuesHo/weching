6b9ec76453702e5147d2d3896ad1272bda9585e6
seungha <frazzle124@gmail.com>
2022-12-28 19:06:15 +0900

    fix : oauth

--- src/passport/strategies/google.ts
+++ src/passport/strategies/google.ts
@@ -6,7 +6,7 @@ import { log } from "../../logger";
 const config = {
   clientID: clientID,
   clientSecret: clientSecret,
-  redirectURL: "http://kdt-sw3-team09.elicecoding.com/auth/google/callback",
+  callbackURL: "/auth/google/callback",
   scope: ["profile", "email"],
 };
 
