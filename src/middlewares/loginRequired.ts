94f4d0f836f5804e10da0684d78bb1eabb7166e4
seungha <frazzle124@gmail.com>
2022-12-29 21:10:59 +0900

    fix : 비회원 메인페이지

--- src/middlewares/loginRequired.ts
+++ src/middlewares/loginRequired.ts
@@ -1,24 +1,24 @@
-import { Request, Response, NextFunction } from 'express';
-import { errorResponse } from '../utils';
-import { log } from '../logger';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import { errorResponse } from "../utils";
+import { log } from "../logger";
+import jwt from "jsonwebtoken";
 
 export function loginRequired(req: Request, res: Response, next: NextFunction) {
-  const userToken = req.headers.authorization?.split(' ')[1];
+  const userToken = req.headers.authorization?.split(" ")[1];
 
-  if (!userToken || userToken === 'null') {
-    log.error('Authorization 토큰 없음');
+  if (!userToken || userToken === "null") {
+    log.error("Authorization 토큰 없음");
     errorResponse(
       res,
-      'FORBIDDEN',
-      '로그인한 유저만 사용할 수 있는 서비스입니다.'
+      "FORBIDDEN",
+      "로그인한 유저만 사용할 수 있는 서비스입니다."
     );
 
     return;
   }
 
   try {
-    const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
     const jwtDecoded = jwt.verify(userToken, secretKey);
     const userId = (<{ userId: string }>jwtDecoded).userId;
     const email = (<{ email: string }>jwtDecoded).email;
@@ -29,7 +29,33 @@ export function loginRequired(req: Request, res: Response, next: NextFunction) {
 
     next();
   } catch (error) {
-    errorResponse(res, 'FORBIDDEN', '정상적인 토큰이 아닙니다.');
+    errorResponse(res, "FORBIDDEN", "정상적인 토큰이 아닙니다.");
+
+    return;
+  }
+}
+
+export function mainUser(req: Request, res: Response, next: NextFunction) {
+  const userToken = req.headers.authorization?.split(" ")[1];
+
+  if (!userToken || userToken === "null") {
+    res.json([]);
+    return;
+  }
+
+  try {
+    const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
+    const jwtDecoded = jwt.verify(userToken, secretKey);
+    const userId = (<{ userId: string }>jwtDecoded).userId;
+    const email = (<{ email: string }>jwtDecoded).email;
+    const status = (<{ status: number }>jwtDecoded).status;
+    req.body.userId = userId;
+    req.body.email = email;
+    req.body.status = status;
+
+    next();
+  } catch (error) {
+    errorResponse(res, "FORBIDDEN", "정상적인 토큰이 아닙니다.");
 
     return;
   }
--- src/routers/mainRouter.ts
+++ src/routers/mainRouter.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils";
 import { mainController, userController } from "../controller";
-import { checkName, loginRequired } from "../middlewares";
+import { checkName, mainUser } from "../middlewares";
 
 export const mainRouter = Router();
 
 mainRouter.get("/", asyncHandler(mainController.mainInfo));
-mainRouter.get("/user", asyncHandler(mainController.userInfo));
+mainRouter.get("/user", mainUser, asyncHandler(mainController.userInfo));
 mainRouter.post(
   "/checkName",
   checkName,
