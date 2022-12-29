07075701eab79f9df53f2f80fb8906575a0537ff
seungha <frazzle124@gmail.com>
2022-12-29 21:02:43 +0900

    fix : 비회원 메인페이지

--- src/routers/mainRouter.ts
+++ src/routers/mainRouter.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import { asyncHandler } from '../utils';
-import { mainController, userController } from '../controller';
-import { checkName, loginRequired } from '../middlewares';
+import { Router } from "express";
+import { asyncHandler } from "../utils";
+import { mainController, userController } from "../controller";
+import { checkName, loginRequired } from "../middlewares";
 
 export const mainRouter = Router();
 
-mainRouter.get('/', asyncHandler(mainController.mainInfo));
-mainRouter.get('/user', loginRequired, asyncHandler(mainController.userInfo));
+mainRouter.get("/", asyncHandler(mainController.mainInfo));
+mainRouter.get("/user", asyncHandler(mainController.userInfo));
 mainRouter.post(
-  '/checkName',
+  "/checkName",
   checkName,
   asyncHandler(userController.isUsingEmail)
 );
