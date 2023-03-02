3609ac349db7481f25db5b3f52c9382e77b03a7d
seungha-o <69295153+AhGnuesHo@users.noreply.github.com>
2023-03-02 15:55:50 +0900

    Delete src/dd/src directory

--- src/dd/src/routers/loginRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginRouter = void 0;
-const express_1 = require("express");
-const controller_1 = require("../controller");
-const utils_1 = require("../utils");
-exports.loginRouter = (0, express_1.Router)();
-exports.loginRouter.post("/", (0, utils_1.asyncHandler)(controller_1.guestController.login));
-//# sourceMappingURL=loginRouter.js.map
